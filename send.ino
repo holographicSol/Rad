9e4066d5c19828eefa8832b4e614d371c9853988
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-06-11 02:54:46 +0100

    Add files via upload

--- send.ino
+++ send.ino
@@ -98,7 +98,7 @@ struct TimeStruct {
 };
 TimeStruct timeData;
 
-// concatinates unix time and micros to make timestamps. requires loop time taken. time resolution is predicated upon loop time.
+// concatinates unix time and micros to make timestamps. requires loop time taken. time resolution is predicated upon loop time and is not meant to be accurate, just unique compared to other times.
 double current_SUBSECOND_UNIXTIME() {
   DateTime time = rtc.now();
   dtostrf((unsigned long)time.unixtime(), 0, 0, timeData.unixtStr);
