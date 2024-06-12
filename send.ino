6c6e8e9aa1503f3ea4cbb35dbac0088d2c221a33
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-06-12 23:07:05 +0100

    Add files via upload

--- send.ino
+++ send.ino
@@ -21,7 +21,7 @@
 // it may be preferrable to have a max count <=100 (cpm 100 considered unsafe to humans) if all you are interested in is reacting to a precise cpm reading within the shortest time you can.
 // if instead you are actually trying to get as precise (arduino is not medical/military grade) a reading as you can at any level of activity then you may increase max count from 10240
 // providing you beleive there is the memory and performance available on the MCU your building for.
-#define max_count 100
+#define max_count 6000
 #define CE_PIN 25 // radio can use tx
 #define CSN_PIN 26 // radio can use rx
 #define GEIGER_PIN 27
